77dd45febe0859873d9473a439eea0a73f611f4b
Simon Atkins <simon@vault.fan>
2023-12-12 22:02:06 +0000

    Install styled-components and add some base styles

---
+++ components/Button.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  padding: 8px;
+  margin: 8px;
+  border: 1px solid black;
+  &:hover {
+    opacity: 0.6;
+  }
+`;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: {
+      displayName: process.env.NODE_ENV === "development",
+      ssr: true,
+      fileName: false,
+      topLevelImportPaths: ["@emotion/react", "@emotion/styled"],
+      cssProp: true,
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
